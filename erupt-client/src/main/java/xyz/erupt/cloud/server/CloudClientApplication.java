61229a3e83d38cac95d4f8e93374a831e86acb48
yuepeng <erupts@126.com>
2021-12-16 00:16:19 +0800

    init cloud module

---
+++ erupt-client/src/main/java/xyz/erupt/cloud/server/CloudClientApplication.java
@@ -0,0 +1,8 @@
+package xyz.erupt.cloud.server;
+
+/**
+ * @author YuePeng
+ * date 2021/12/16 00:15
+ */
+public class CloudClientApplication {
+}
---
+++ erupt-server/src/main/java/xyz/erupt/cloud/client/CloudServerApplication.java
@@ -0,0 +1,8 @@
+package xyz.erupt.cloud.client;
+
+/**
+ * @author YuePeng
+ * date 2021/12/16 00:15
+ */
+public class CloudServerApplication {
+}
